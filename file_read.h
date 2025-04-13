425d1c63f5d8828641ec371b842a810ab3f2c7f8
Primz007 <patelpriyam2006@gmail.com>
2025-04-14 04:57:00 +0530

    files dependencies

---
+++ file_read.h
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include "linked_list.h"
+#include <stdlib.h>
+#include <ctype.h>
+#ifndef FILE_READ_H
+#define FILE_READ_H
+//max size of file (how many charector change as fit)
+#define size_of_file 262144*8
+
+void do_Nothing(){
+    //As the function says do Nothing
+}
+//Read the file first
+node* fileRead(char* filename){
+    
+    //I WILL INITIALISE LINKED LIST HERE
+    node *head = NULL;//INITIALISE TO NULL SO C DOESNT GET A CHANCE TO SEGFAULT ITS FAVOURITE THING TO DO
+    // POINTING TO HEAD WHICH IS NOT NULL SEGFAULTS CUZ FUCK U
+    //END
+    FILE *file_pointer = fopen(filename, "r");
+    //GET THE FAIL CONDITION CUZ WHY NOT C LOVES TO FAIL RETARDED POS
+    if(file_pointer == NULL){
+    perror("Welcome to C");
+    return head;
+    }
+    //ALLOCATE 16KB FOR EACH LINE
+    char* buffer = calloc(size_of_file,sizeof(char));
+    // memset(buffer, 0, size_of_file);//setting memory of entire buffer to 0 before so garbage values are impossible to send to linked list
+
+    //READ THE LINE AND TERMINATE WHEN FGETS RETURN NULL POINTER
+    while(fgets(buffer, size_of_file, file_pointer)){
+        // printf("RAWDOg: %s\n", buffer);
+        //FGETS KEEPS \N AT END OF LINE I REPLACE WITH SPACE
+        buffer[strcspn(buffer, "\n")] = ' ';
+        //APPLY FOR EACH WORD
+        //required as else tokenisation process may end 
+        //up including it
+
+
+        // IGNORING COMMENT and #include
+        //strncmp compares two charectors of string we pass with the one we wanna compare
+        if(strstr(buffer, "#")){
+            buffer[strcspn(buffer, "#")] = '\0';
+        }
+        if (strstr(buffer, "//"))
+        {   
+            //we cant skip the whole thing if code is like this: code // comment
+            //in this case i'll delete last parts for multiline comment
+            //who uses it anyways wont handle those edgecases
+            // its already 2:30 am damn
+            buffer[strcspn(buffer, "//")] = '\0';
+            // remove the thing when // starts so code before // remains!!!
+            
+        }
+        // for */ untill we find */ it will skip adding nodes
+        //skips inline */ comment */ but skips code on it
+        if(strstr(buffer, "/*") && strstr(buffer, "*/")){
+            
+            continue;
+            //if multiline
+            /*
+            lol
+            */
+           //it will check for starting point /* and untill it doesnt find */ 
+           // it skips lines, after finding break and skip this line
+           //but it will skip code after it
+        }
+        else if(strstr(buffer, "/*")){
+            while(fgets(buffer, size_of_file, file_pointer)){
+                if(strstr(buffer, "*/"))
+                {
+                    break;}
+                    continue;
+            }
+            continue;
+        }
+        //Now we'll start tokenising,
+            //STD STRTOK METHOD, START WITH BUFFER IT REMEMBER WHERE IT LEFT OFF SO PASS NULL
+            // AND IT CONTINUE TILL NULL POINTER
+            char* token = strtok(buffer, " ,.-\n!:;(){}[]\"/*-+=|");
+            while(token != NULL){
+                if(strlen(token) == 0){
+                   // token = NULL; -> this never works as ur messing with strtok advanced tracking system
+                   token = strtok(NULL,  " ,.-\n!:;(){}[]\"/*-+=|");
+                    continue;
+                    // so that c doesnt seg fault when a stupid ass invisible charector slips in
+                    //yk that c likes to segfault so it enters it own invisble char 
+                    // to segfault genius design right
+                }
+            //C IS C NOT ASSIGN STRING TO ARRAY GENIUS DESIGN RIGHT SO USES STRCPY
+            node* p1 = malloc(sizeof(node));
+            if (p1 == NULL) {
+                perror("Download more ram nub");
+                exit(1);
+            }
+            
+            char lower[WORD_LENGTH] = {0};
+             //strcpy(lower, token);
+             
+            strncpy(lower, token, WORD_LENGTH - 1); // strncpy to prevent overflow for eg if lenght
+            //exceeds 128 so it would truncate till max length and add \0 safely copies i.e.
+            //strcpy doesnt add \0 at end so it included garbage shit which overflows and seg faults
+            //DO NOT I REPEAT DO NOT TOUCH THIS strncpy pls
+            //fix passing garbage
+            //strncpy does not null-terminate the string if the source is longer than or equal to the destination size.
+            lower[WORD_LENGTH - 1] = '\0';
+            for(int i = 0; i <strlen(lower); i++){//length of token undefined may exceed 128 so yeah
+                lower[i] = tolower(lower[i]);
+            }
+
+            strcpy( p1->word, lower);
+            p1->next = NULL;
+            //debugging if the token is garbage or not
+            //  printf("TOKEN: '%s' (first char ASCII: %d)\n", token, (int)token[0]);
+
+            // printf("TOKEN: %s\n", lower);
+           
+            add_at_head(&head, p1);
+            
+            token = strtok(NULL, " ,.-\n!:;(){}[]\"/*-+=|");
+            }
+        
+        
+    }
+    
+    
+    free(buffer);
+    return head;
+}
+
+
+#endif
---
+++ linked_list.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define WORD_LENGTH 128
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+//STANDARD LINKED LIST THINGS YOU KNOW THAT DONT YOU
+typedef struct node{
+    char word[WORD_LENGTH];
+    struct node* next;
+}node;
+
+//This function is useless lmao as u cannot directly assign a string to an array of char
+//genius design
+node* create_node(char* word){
+    node* new = malloc(sizeof(node));
+    new->next = NULL;
+    strcpy(new->word, word);
+    return new;
+}
+
+void add_at_head(node** head, node* new){
+    new->next = *head;
+    *head = new;   
+}
+
+//FOR DEBUGGING PURPOSE REMOVE LATER
+// void debug_printlist(node *head){
+//     if(head == NULL){
+//         return;
+//     }
+//     while(head != NULL){
+//         printf("%s ", head->word);
+//         head = head->next;
+//     }
+// }
+
+// void debug_printlist(node *head){
+//     while(head != NULL){
+//         if (head->word[0] != '\0')
+//             printf("[%s] -> ", head->word);
+//         else
+//             printf("[???] -> ");
+//         head = head->next;
+//     }
+//     printf("NULL\n");
+// }
+
+void debug_printlist(node *head){
+    while(head != NULL){
+        printf("%s ", head->word);
+        head = head->next;
+    }
+}
+
+
+
+#endif
